94c92542564817311e5e776731ed69929f345b7e
DonatPataki <43422068+DonatPataki@users.noreply.github.com>
2019-03-12 10:22:40 +0100

    Create deklaracio.c

---
+++ prog1/deklaracio.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int a; //egész
+    int *b; //egészre mutató mutató
+    int &&c = 10; //egész referenciája
+    int d[1];
+    int* &&e = d; //egészek tomjének referenciája
+    int *f[1]; //egészre mutató mutatók tömbje
+    int *fuggveny(); //egészre mutató mutatót visszaadó függvény
+    int (*g)(int); //egészre mutató mutatót visszaadó függvényre mutató mutató
+    //egészet visszaadó és két egészet kapó függvényre mutató mutatót visszaadó, egészet kapó függvény
+    //függvénymutató egy egészet visszaadó és két egészet kapó függvényre mutató mutatót visszaadó, egészet kapó függvényre
+
+    return 0;
+}
