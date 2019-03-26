debc99678bdf7e2bc79f3483a9c621aff7eea476
DonatPataki <43422068+DonatPataki@users.noreply.github.com>
2019-03-26 13:23:52 +0100

    Update deklaracio.c

--- prog1/deklaracio.c
+++ prog1/deklaracio.c
@@ -8,8 +8,8 @@ int main()
     int *f[1]; //egészre mutató mutatók tömbje
     int *fuggveny(); //egészre mutató mutatót visszaadó függvény
     int (*g)(int); //egészre mutató mutatót visszaadó függvényre mutató mutató
-    //egészet visszaadó és két egészet kapó függvényre mutató mutatót visszaadó, egészet kapó függvény
-    //függvénymutató egy egészet visszaadó és két egészet kapó függvényre mutató mutatót visszaadó, egészet kapó függvényre
+    int (*F) (int, int));//egészet visszaadó és két egészet kapó függvényre mutató mutatót visszaadó, egészet kapó függvény
+    int (*(*G) (int)) (int, int);//függvénymutató egy egészet visszaadó és két egészet kapó függvényre mutató mutatót visszaadó, egészet kapó függvényre
 
     return 0;
 }
