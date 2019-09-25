777feb851eb7f40d888a2c3233958ca5b365b8aa
DonatPataki <43422068+DonatPataki@users.noreply.github.com>
2019-09-25 09:32:19 +0200

    Update z3a7.java

--- prog2/bhax/thematic_tutorials/bhax_textbook/src/arroway/z3a7.java
+++ prog2/bhax/thematic_tutorials/bhax_textbook/src/arroway/z3a7.java
@@ -1,6 +1,18 @@
 import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.lang.Exception;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileWriter;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
 
 final class Node {
   public Node(char c) {
@@ -259,10 +271,26 @@ final class Node {
                                                   new java.io.FileWriter(args[3])));
 
         lzwbt.printTree(printWriter);
+        output.print("depth " + theTree.getDepth () + "\n");
+        output.print("mean " + theTree.getMean () + "\n");
+        output.print("var " + theTree.getVariance () + "\n");
 
-        printWriter.printf("depth = %d%n", lzwbt.getMaxDepth());
-        printWriter.printf("mean = %f%n",  lzwbt.mean());
-        printWriter.printf("var = %f%n",   lzwbt.variance());
+        output.close();
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head><title>LZWBinFa</title></head>");
+        out.println("<body>");
+        out.println("<h1>Binfa</h1>");
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("o")))))
+        {
+            for(String line; (line = br.readLine()) != null; ) 
+            {
+                out.println("<p>" + line + "</p>");
+            }
+        }
+        out.println("</body></html>");
+
 
         printWriter.flush();
       }
