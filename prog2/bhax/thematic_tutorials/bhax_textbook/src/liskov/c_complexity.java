92eb3f71171d9a30a61d413d2eb3a9efff9712c3
DonatPataki <43422068+DonatPataki@users.noreply.github.com>
2019-10-01 21:40:06 +0200

    Add files via upload

---
+++ prog2/bhax/thematic_tutorials/bhax_textbook/src/liskov/c_complexity.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class c_complexity {
+	public static void main(String[] args) {
+		System.out.println("Wish to follow yoda conditions do you, hmm");
+		Scanner input = new Scanner(System.in);
+		String answer = input.nextLine();
+		if(answer.equals("yes")) {
+		System.out.println("Yes, hmmm.");
+		} else {
+			Boolean myBoolean = true;
+			if (myBoolean = null) { /* ... */ }
+		}
+	System.out.println("Good, you followed my contion I sense.  Herh herh herh.");
+	}
+}
