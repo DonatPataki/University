f08ceb3f6a15553bff13e1e14dbbbc39b621b6c0
DonatPataki <43422068+DonatPataki@users.noreply.github.com>
2019-10-01 19:16:00 +0200

    Create liskov.java

---
+++ prog2/bhax/thematic_tutorials/bhax_textbook/src/liskov/liskov.java
@@ -0,0 +1,13 @@
+class bird{
+    public void fly(){
+
+    }
+}
+class duck extends bird{}
+class ostrich extends bird{}
+public class liskov {
+
+    public static void main(String[] args) {
+
+    }
+}
