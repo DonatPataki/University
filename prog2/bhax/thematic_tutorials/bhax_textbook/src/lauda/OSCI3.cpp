aef66043c3eab16cc9eda7d92fbe5ffe5cd79153
DonatPataki <43422068+DonatPataki@users.noreply.github.com>
2019-12-03 18:51:16 +0100

    Update OSCI3.cpp

--- prog2/bhax/thematic_tutorials/bhax_textbook/src/lauda/OSCI3.cpp
+++ prog2/bhax/thematic_tutorials/bhax_textbook/src/lauda/OSCI3.cpp
@@ -1,6 +1,60 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "../glm/glm.hpp"
+#include "../glm/gtc/matrix_transform.hpp"
+
+static float s = 1.0f;
+static double xpress, ypress, x, y, previous_x, previous_y;
+static glm::vec3 translation(0, 0, 0);
+static bool lbutton_down = false;
+
+static unsigned int compileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = &source[0];
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "error" << (type == GL_VERTEX_SHADER ? " vertex" : " fragment") << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+}
 
 int main(void)
 {
@@ -12,44 +66,75 @@ int main(void)
 
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "OSCI3", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "OSCI3", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
+
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 	if (glewInit() != GLEW_OK)
 		return -1;
 
-	float positions[] = {
-		-0.5f, -0.5f,
-		 0.5f,  0.5f,
-		 0.3f, 0.4f,
-		 1.0f, 1.0f,
-		 0.2f, -0.4f,
-		 -0.9f, -0.8f,
-		 -0.7f, 0.0f,
-		 0.2f, 0.0f
-	};
+	float positions[] =
+#include"poPoints.txt"
+		;
+	unsigned int indexes[] = 
+#include"poIndexes.txt"
+		;
 
-	unsigned int indexes[] = {0, 2, 2, 2, 5, 4};
-		
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;"
+		"\n"
+		"uniform mat4 u_MVP;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = u_MVP * position;\n"
+		"}\n";
+
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;"
+		"\n"
+		"uniform vec4 u_Color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = u_Color;\n"
+		"}\n";
+	unsigned int shader = createShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
+
+	int location = glGetUniformLocation(shader, "u_Color");
+	glUniform4f(location, 0.2f, 0.5f, 0.8f, 1.0f);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
+		glClearColor(0.752f, 0.752f, 0.752f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glm::mat4 proj = glm::ortho(475308671.0f, 475998139.0f, 216029324.0f, 216602269.0f, -1.0f, 1.0f);
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+		glm::mat4 mvp = proj * view * model;
+		glUniformMatrix4fv(glGetUniformLocation(shader, "u_MVP"), 1, GL_FALSE, &mvp[0][0]);
+
+		glLineWidth(3);
 		glEnableClientState(GL_VERTEX_ARRAY);
 		glVertexPointer(2, GL_FLOAT, 0, positions);
-			glDrawArrays(GL_LINE_STRIP, indexes[0], indexes[1]);
-			glDrawArrays(GL_LINE_STRIP, indexes[2], indexes[3]);
-			glDrawArrays(GL_LINE_STRIP, indexes[4], indexes[5]);
+		for (int i = 0; i < 32; i++)
+			glDrawArrays(GL_LINE_STRIP, indexes[i++], indexes[i]);
 		glDisableClientState(GL_VERTEX_ARRAY);
 
 		/* Swap front and back buffers */
@@ -59,6 +144,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
